7dca20a9968d1d40a0795db23c238850708a3e9b
Artazione <jules.lefevre@etudiant.univ-reims.fr>
2022-09-27 08:50:25 +0200

    initial commit

--- Mavigation.py
+++ Mavigation.py
@@ -3,29 +3,57 @@ import pyautogui
 import webbrowser
 import math
 
+BLACK = pygame.Color('black')
+WHITE = pygame.Color('white')
+
+pyautogui.FAILSAFE = False
+
+class TextPrint(object):
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 20)
+
+    def tprint(self, screen, textString):
+        textBitmap = self.font.render(textString, True, BLACK)
+        screen.blit(textBitmap, (self.x, self.y))
+        self.y += self.line_height
+
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+
+    def indent(self):
+        self.x += 10
+
+    def unindent(self):
+        self.x -= 10
+
+
 pygame.init()
 
-# Set the width and height of the screen (width, height).
+#Defini la hauteur et la largeur de l'affichage(largeur, hauteur).
 screen = pygame.display.set_mode((400, 600))
 
-pygame.display.set_caption("gamepad")
+pygame.display.set_caption("Manette")
 
-# Loop until the user clicks the close button.
 done = False
 
-# Used to manage how fast the screen updates.
+# Defini la fréquence de rafraichissement
 clock = pygame.time.Clock()
 
-# Initialize the joysticks.
+# Initialise les joysticks.
 pygame.joystick.init()
 
-# -------- Main Program Loop -----------
+# Preparation pour l'affichage
+textPrint = TextPrint()
+
+# -------- Boucle du programme -----------
 while not done:
     #
-    # EVENT PROCESSING STEP
+    # Etape des evenements:
     #
-    # Possible joystick actions: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN,
-    # JOYBUTTONUP, JOYHATMOTION
+    # Action possible sur les joysticks: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN, JOYBUTTONUP, JOYHATMOTION.
     for event in pygame.event.get(): # User did something.
         if event.type == pygame.QUIT: # If user clicked close.
             done = True # Flag that we are done so we exit this loop.
@@ -34,56 +62,94 @@ while not done:
         elif event.type == pygame.JOYBUTTONUP:
             print("Joystick button released.")
 
-# Get count of joysticks.
+    #
+    # Apparition de l'affichage
+    #
+    # Rempli l'affichage de blanc, a faire avant les autres commandes ou elle seront effaces par cette commande
+    screen.fill(WHITE)
+    textPrint.reset()
+
+    # Compte les joysticks.
     joystick_count = pygame.joystick.get_count()
 
-    # For each joystick:
+    textPrint.tprint(screen, "Nombre de joysticks: {}".format(joystick_count))
+    textPrint.indent()
+
+    # Pour chaque joystick:
     for i in range(joystick_count):
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
 
-axes = joystick.get_numaxes()
+        textPrint.tprint(screen, "Joystick {}".format(i))
+        textPrint.indent()
 
-    # For each joystick:
-for i in range(joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
+        # Recupere le nom du périphérique.
+        name = joystick.get_name()
+        textPrint.tprint(screen, "Nom du peripherique: {}".format(name))
 
-        # Usually axis run in pairs, up/down for one, and left/right for
-        # the other.
+        # Les axes des joysticks fonctionnent par paires : haut/bas et gauche/droite
         axes = joystick.get_numaxes()
+        textPrint.tprint(screen, "Nombre  d'axes: {}".format(axes))
+        textPrint.indent()
 
-
-
-for i in range(axes):
+        for i in range(axes):
             axis = joystick.get_axis(i)
+            textPrint.tprint(screen, "Axe {} valeur: {:>6.3f}".format(i, axis))
             xx=0
             yy=0
-            if i==0:
-                xx=axis*1366
-                pyautogui.moveRel(xx/10,0,duration=1)
-            elif i==1:
-                yy=axis*768
-                pyautogui.moveRel(0,yy/10,duration=1)
-            elif i==3 and axis < 0.2:
-                pyautogui.hscroll((2))
-            elif i==3 and axis > 0.2:
-                pyautogui.hscroll(-(2))
-            elif i==4 and axis < 0.2:
-                pyautogui.scroll((2))
-            elif i==4 and axis > 0.2:
-                pyautogui.scroll(-(2))
-            elif i==2 and (axis!=-1 and axis!=0):
-                pyautogui.hotkey('ctrl','tab')
-                print("aaaa")
-            elif i==5 and (axis!=-1 and axis!=0):
-                pyautogui.hotkey('ctrl','shift','tab')
+            if i==3 and axis <= -0.9:
+                pyautogui.scroll(100)
+            elif i==3 and axis >=0.9:
+                pyautogui.scroll(-100)
+            elif i==3 and axis <= -0.5:
+                pyautogui.scroll(50)
+            elif i==3 and axis >=0.5:
+                pyautogui.scroll(-50)
+            elif i==3 and axis <= -0.2:
+                pyautogui.scroll(20)
+            elif i==3 and axis >=0.2:
+                pyautogui.scroll(-20)
+            
+        textPrint.unindent()
 
-clock.tick(20)
+        buttons = joystick.get_numbuttons()
+        textPrint.tprint(screen, "Nombre de boutons: {}".format(buttons))
+        textPrint.indent()
 
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
+        for i in range(buttons):
+            button = joystick.get_button(i)
+            textPrint.tprint(screen,
+                             "Button {:>2} value: {}".format(i, button))
+            if i==8 and button==1:
+                webbrowser.open("https://google.com/",2)
+            elif i==4 and button==1:
+                pyautogui.click()
+            elif i==0 and button==1:
+                pyautogui.click(button="left")
+ 
+        textPrint.unindent()
+
+        hats = joystick.get_numhats()
+        textPrint.tprint(screen, "Nombre de boutons de la croix directionnel: {}".format(hats))
+        textPrint.indent()
+
+        # Valeur de la croix directionnel, binaire : soit pressé ou non (1 ou 0, format :(x, y)).
+        for i in range(hats):
+            hat = joystick.get_hat(i)
+            textPrint.tprint(screen, "Boutton {} valeur: {}".format(i, str(hat)))
+        textPrint.unindent()
+
+        textPrint.unindent()
+
+    #
+    # Tout code de dessin doit etre au dessus de ce commentaire
+    #
+
+    # Met a jour la fenetre avec ce que l'on a dessine
+    pygame.display.flip()
+
+    #Limite a 20 images par seconde.
+    clock.tick(20)
+
+# Ferme la fenetre et le programme.
 pygame.quit()
-
-
